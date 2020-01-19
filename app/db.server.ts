6162c2bb6a058ef7fe6e05ae13ca0cad50b922a1
CodeWithSimple <120245566+CodeWithSimple@users.noreply.github.com>
2020-01-19 19:23:36 +0000

    Extended API

---
+++ app/db.server.ts
@@ -0,0 +1,10 @@
+import { PrismaClient } from "@prisma/client";
+
+let prisma: PrismaClient;
+
+declare global {
+  var __db__: PrismaClient;
+}
+
+// this is needed because in development we don't want to restart
+// the server with every change, 
