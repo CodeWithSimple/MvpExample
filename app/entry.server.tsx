62fa4c474efd265c6e3ac5b603ebdd086b1846c1
CodeWithSimple <120245566+CodeWithSimple@users.noreply.github.com>
2020-02-09 10:39:39 +0000

    Adding .gitattributes

--- app/entry.server.tsx
+++ app/entry.server.tsx
@@ -3,4 +3,19 @@ import { RemixServer } from "remix";
 import type { EntryContext } from "remix";
 
 export default function handleRequest(
-  request
+  request: Request,
+  responseStatusCode: number,
+  responseHeaders: Headers,
+  remixContext: EntryContext
+) {
+  const markup = renderToString(
+    <RemixServer context={remixContext} url={request.url} />
+  );
+
+  responseHeaders.set("Content-Type", "text/html");
+
+  return new Response("<!DOCTYPE html>" + markup, {
+    status: responseStatusCode,
+    headers: responseHeaders,
+  });
+}
