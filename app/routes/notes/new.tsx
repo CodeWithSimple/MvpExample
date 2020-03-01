3e861bf18e6d48def0b740a1f99338266efdb590
CodeWithSimple <120245566+CodeWithSimple@users.noreply.github.com>
2020-03-01 18:17:40 +0000

    Clean translations

--- app/routes/notes/new.tsx
+++ app/routes/notes/new.tsx
@@ -13,4 +13,30 @@ type ActionData = {
   };
 };
 
-export co
+export const action: ActionFunction = async ({ request }) => {
+  const userId = await requireUserId(request);
+
+  const formData = await request.formData();
+  const title = formData.get("title");
+  const body = formData.get("body");
+
+  if (typeof title !== "string" || title.length === 0) {
+    return json<ActionData>(
+      { errors: { title: "Title is required" } },
+      { status: 400 }
+    );
+  }
+
+  if (typeof body !== "string" || body.length === 0) {
+    return json<ActionData>(
+      { errors: { body: "Body is required" } },
+      { status: 400 }
+    );
+  }
+
+  const note = await createNote({ title, body, userId });
+
+  return redirect(`/notes/${note.id}`);
+};
+
+export default function 
