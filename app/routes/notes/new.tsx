fb98803e502d8c3c23395bab0bac44d54d9e7f9a
CodeWithSimple <120245566+CodeWithSimple@users.noreply.github.com>
2020-03-08 08:33:57 +0000

    remove a console.log

--- app/routes/notes/new.tsx
+++ app/routes/notes/new.tsx
@@ -39,4 +39,32 @@ export const action: ActionFunction = async ({ request }) => {
   return redirect(`/notes/${note.id}`);
 };
 
-export default function 
+export default function NewNotePage() {
+  const actionData = useActionData() as ActionData;
+  const titleRef = React.useRef<HTMLInputElement>(null);
+  const bodyRef = React.useRef<HTMLTextAreaElement>(null);
+
+  React.useEffect(() => {
+    if (actionData?.errors?.title) {
+      titleRef.current?.focus();
+    } else if (actionData?.errors?.body) {
+      bodyRef.current?.focus();
+    }
+  }, [actionData]);
+
+  return (
+    <Form
+      method="post"
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        gap: 8,
+        width: "100%",
+      }}
+    >
+      <div>
+        <label className="flex w-full flex-col gap-1">
+          <span>Title: </span>
+          <input
+            ref={titleRef}
+    
