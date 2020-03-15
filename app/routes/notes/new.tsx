1f97fb27f7d498305a0b436d0d47e4eeefd718de
CodeWithSimple <120245566+CodeWithSimple@users.noreply.github.com>
2020-03-15 07:02:07 +0000

    Update Tests

--- app/routes/notes/new.tsx
+++ app/routes/notes/new.tsx
@@ -67,4 +67,22 @@ export default function NewNotePage() {
           <span>Title: </span>
           <input
             ref={titleRef}
-    
+            name="title"
+            className="flex-1 rounded-md border-2 border-blue-500 px-3 text-lg leading-loose"
+            aria-invalid={actionData?.errors?.title ? true : undefined}
+            aria-errormessage={
+              actionData?.errors?.title ? "title-error" : undefined
+            }
+          />
+        </label>
+        {actionData?.errors?.title && (
+          <Alert className="pt-1 text-red-700" id="title=error">
+            {actionData.errors.title}
+          </Alert>
+        )}
+      </div>
+
+      <div>
+        <label className="flex w-full flex-col gap-1">
+          <span>Body: </span>
+         
