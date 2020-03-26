b2f0f6f6a9621868b280b4658aea5cc03604a969
CodeWithSimple <120245566+CodeWithSimple@users.noreply.github.com>
2020-03-26 12:56:33 +0000

    fix include path

--- app/routes/notes.tsx
+++ app/routes/notes.tsx
@@ -12,4 +12,28 @@ type LoaderData = {
 export const loader: LoaderFunction = async ({ request }) => {
   const userId = await requireUserId(request);
   const noteListItems = await getNoteListItems({ userId });
-  return json<LoaderD
+  return json<LoaderData>({ noteListItems });
+};
+
+export default function NotesPage() {
+  const data = useLoaderData() as LoaderData;
+  const user = useUser();
+
+  return (
+    <div className="flex h-full min-h-screen flex-col">
+      <header className="flex items-center justify-between bg-slate-800 p-4 text-white">
+        <h1 className="text-3xl font-bold">
+          <Link to=".">Notes</Link>
+        </h1>
+        <p>{user.email}</p>
+        <Form action="/logout" method="post">
+          <button
+            type="submit"
+            className="rounded bg-slate-600 py-2 px-4 text-blue-100 hover:bg-blue-500 active:bg-blue-600"
+          >
+            Logout
+          </button>
+        </Form>
+      </header>
+
+      <main className="flex h-fu
