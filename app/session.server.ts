1b3e5d71c34356d9359806e08da4eb10ad2c4211
CodeWithSimple <120245566+CodeWithSimple@users.noreply.github.com>
2020-03-29 11:55:17 +0000

    ASoC: wm2000: Correct register size

--- app/session.server.ts
+++ app/session.server.ts
@@ -10,4 +10,27 @@ export const sessionStorage = createCookieSessionStorage({
   cookie: {
     name: "__session",
     httpOnly: true,
-   
+    maxAge: 0,
+    path: "/",
+    sameSite: "lax",
+    secrets: [process.env.SESSION_SECRET],
+    secure: process.env.NODE_ENV === "production",
+  },
+});
+
+const USER_SESSION_KEY = "userId";
+
+export async function getSession(request: Request) {
+  const cookie = request.headers.get("Cookie");
+  return sessionStorage.getSession(cookie);
+}
+
+export async function getUserId(request: Request): Promise<string | undefined> {
+  const session = await getSession(request);
+  const userId = session.get(USER_SESSION_KEY);
+  return userId;
+}
+
+export async function getUser(request: Request): Promise<null | User> {
+  const userId = await getUserId(request);
+  if (userId
