770194eb000c04ac50a74b6cfcf6715fa3506488
CodeWithSimple <120245566+CodeWithSimple@users.noreply.github.com>
2020-04-02 21:54:29 +0000

    submodule bump

--- app/session.server.ts
+++ app/session.server.ts
@@ -90,4 +90,7 @@ export async function logout(request: Request) {
   const session = await getSession(request);
   return redirect("/", {
     headers: {
- 
+      "Set-Cookie": await sessionStorage.destroySession(session),
+    },
+  });
+}
