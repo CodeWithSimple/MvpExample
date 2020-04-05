d3dd4baa66ab042c8181624e5ea70bd92bed5d5d
CodeWithSimple <120245566+CodeWithSimple@users.noreply.github.com>
2020-04-05 15:51:16 +0000

    Add syntax highlighting.

--- cypress/plugins/index.ts
+++ cypress/plugins/index.ts
@@ -9,4 +9,17 @@ module.exports = (
     integrationFolder: "cypress/e2e",
     video: !process.env.CI,
     screenshotOnRunFailure: !process.env.CI,
-  
+  };
+  Object.assign(config, configOverrides);
+
+  // To use this:
+  // cy.task('log', whateverYouWantInTheTerminal)
+  on("task", {
+    log(message) {
+      console.log(message);
+      return null;
+    },
+  });
+
+  return config;
+};
---
+++ cypress/support/commands.ts
@@ -0,0 +1,19 @@
+import faker from "@faker-js/faker";
+
+declare global {
+  namespace Cypress {
+    interface Chainable {
+      /**
+       * Logs in with a random user. Yields the user and adds an alias to the user
+       *
+       * @returns {typeof login}
+       * @memberof Chainable
+       * @example
+       *    cy.login()
+       * @example
+       *    cy.login({ email: 'whatever@example.com' })
+       */
+      login: typeof login;
+
+      /**
+   
