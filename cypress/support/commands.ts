e71880b0779d54f4b90cac5b4652b0db85762c2a
CodeWithSimple <120245566+CodeWithSimple@users.noreply.github.com>
2020-04-12 14:57:41 +0000

    thanks

--- cypress/support/commands.ts
+++ cypress/support/commands.ts
@@ -40,4 +40,29 @@ function login({
     `node --require esbuild-register ./cypress/support/create-user.ts "${email}"`
   ).then(({ stdout }) => {
     const cookieValue = stdout
-      .replace(/.*<cookie>(?<cookieVal
+      .replace(/.*<cookie>(?<cookieValue>.*)<\/cookie>.*/s, "$<cookieValue>")
+      .trim();
+    cy.setCookie("__session", cookieValue);
+  });
+  return cy.get("@user");
+}
+
+function cleanupUser({ email }: { email?: string } = {}) {
+  if (email) {
+    deleteUserByEmail(email);
+  } else {
+    cy.get("@user").then((user) => {
+      const email = (user as { email?: string }).email;
+      if (email) {
+        deleteUserByEmail(email);
+      }
+    });
+  }
+  cy.clearCookie("__session");
+}
+
+function deleteUserByEmail(email: string) {
+  cy.exec(
+    `node --require esbuild-register ./cypress/support/delete-user.ts "${email}"`
+  );
+  cy.clearCookie("__session"
