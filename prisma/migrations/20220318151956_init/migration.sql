a309a1eac42188513130dbd4e48e5792ebe68dec
CodeWithSimple <120245566+CodeWithSimple@users.noreply.github.com>
2023-03-28 19:50:01 +0000

    Update login.php

--- prisma/migrations/20220318151956_init/migration.sql
+++ prisma/migrations/20220318151956_init/migration.sql
@@ -35,4 +35,5 @@ CREATE UNIQUE INDEX "Password_userId_key" ON "Password"("userId");
 -- AddForeignKey
 ALTER TABLE "Password" ADD CONSTRAINT "Password_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
 
--- AddFor
+-- AddForeignKey
+ALTER TABLE "Note" ADD CONSTRAINT "Note_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
