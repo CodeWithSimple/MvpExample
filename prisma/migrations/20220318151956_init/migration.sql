7d591e2035f79640646754ce2627b9df34e31896
CodeWithSimple <120245566+CodeWithSimple@users.noreply.github.com>
2023-03-28 10:10:06 +0000

    Add basic example

---
+++ prisma/migrations/20220318151956_init/migration.sql
@@ -0,0 +1,13 @@
+-- CreateTable
+CREATE TABLE "User" (
+    "id" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Password" (
+    "hash" TEXT
