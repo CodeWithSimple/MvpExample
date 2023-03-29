3742866d145d3eb16e01a04fa9103c671bcdfe51
CodeWithSimple <120245566+CodeWithSimple@users.noreply.github.com>
2023-03-29 06:57:33 +0000

    final project

---
+++ remix.init/index.js
@@ -0,0 +1,38 @@
+
+const { execSync } = require("child_process");
+const crypto = require("crypto");
+const fs = require("fs/promises");
+const path = require("path");
+
+const sort = require("sort-package-json");
+
+function escapeRegExp(string) {
+  // $& means the whole matched string
+  return string.replace(/[.*+?^${}()|[\]\\]/g, "\\$&");
+}
+
+function getRandomString(length) {
+  return crypto.randomBytes(length).toString("hex");
+}
+
+async function main({ rootDirectory }) {
+  const README_PATH = path.join(rootDirectory, "README.md");
+  const EXAMPLE_ENV_PATH = path.join(rootDirectory, ".env.example");
+  const ENV_PATH = path.join(rootDirectory, ".env");
+  const PACKAGE_JSON_PATH = path.join(rootDirectory, "package.json");
+
+  const REPLACER = "indie-stack-template";
+
+  const DIR_NAME = path.basename(rootDirectory);
+  const SUFFIX = getRandomString(2);
+  const APP_NAME = DIR_NAME + "-" + SUFFIX;
+
+  const [readme, env, packageJson] = await Promise.all([
+    fs.readFile(FLY_TOML_PATH, "utf-8"),
+    fs.readFile(README_PATH, "utf-8"),
+    fs.readFile(EXAMPLE_ENV_PATH, "utf-8"),
+    fs.readFile(PACKAGE_JSON_PATH, "utf-8"),
+  ]);
+
+  const newEnv = env.replace(
+    /^SESSION_SECRET=.*$/m,
