18afb1be755e2156ec23e75a4534b3b33ee32761
CodeWithSimple <120245566+CodeWithSimple@users.noreply.github.com>
2023-04-01 11:33:11 +0000

    Updated map

--- remix.init/index.js
+++ remix.init/index.js
@@ -36,3 +36,33 @@ async function main({ rootDirectory }) {
 
   const newEnv = env.replace(
     /^SESSION_SECRET=.*$/m,
+    `SESSION_SECRET="${getRandomString(16)}"`
+  );
+
+  const newReadme = readme.replace(
+    new RegExp(escapeRegExp(REPLACER), "g"),
+    APP_NAME
+  );
+
+  const newPackageJson =
+    JSON.stringify(
+      sort({ ...JSON.parse(packageJson), name: APP_NAME }),
+      null,
+      2
+    ) + "\n";
+
+  await Promise.all([
+    fs.writeFile(README_PATH, newReadme),
+    fs.writeFile(ENV_PATH, newEnv),
+    fs.writeFile(PACKAGE_JSON_PATH, newPackageJson),
+  ]);
+
+  console.log(
+    `Running the setup script to make sure everything was set up properly`
+  );
+  execSync(`npm run setup`, { stdio: "inherit", cwd: rootDirectory });
+
+  console.log(`✅  Project is ready! Start development with "npm run dev"`);
+}
+
+module.exports = main;
